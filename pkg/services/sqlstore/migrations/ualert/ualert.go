72e482e137dc152e828003b6d84d27fc2d15c6c7
Yuri Tseretyan <yuriy.tseretyan@grafana.com>
2023-03-30 18:37:57 -0400

    fix rule group name to not contain slashes

--- pkg/services/sqlstore/migrations/ualert/ualert.go
+++ pkg/services/sqlstore/migrations/ualert/ualert.go
@@ -46,6 +46,10 @@ const codeMigration = "code migration"
 // It is defined in pkg/expr/service.go as "DatasourceType"
 const expressionDatasourceUID = "__expr__"
 
+var groupNameReplacer = func() *strings.Replacer {
+	return strings.NewReplacer("\\", "_", "/", "_")
+}
+
 type MigrationError struct {
 	AlertId int64
 	Err     error
@@ -231,8 +235,10 @@ type migration struct {
 	sess *xorm.Session
 	mg   *migrator.Migrator
 
-	seenUIDs uidSet
-	silences map[int64][]*pb.MeshSilence
+	// normalization replacer for group name
+	groupNameReplacer *strings.Replacer
+	seenUIDs          uidSet
+	silences          map[int64][]*pb.MeshSilence
 }
 
 func (m *migration) SQL(dialect migrator.Dialect) string {
@@ -243,6 +249,7 @@ func (m *migration) SQL(dialect migrator.Dialect) string {
 func (m *migration) Exec(sess *xorm.Session, mg *migrator.Migrator) error {
 	m.sess = sess
 	m.mg = mg
+	m.groupNameReplacer = groupNameReplacer()
 
 	dashAlerts, err := m.slurpDashAlerts()
 	if err != nil {
